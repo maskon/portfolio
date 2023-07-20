292537712571bdd97515b719353cd9a1d6d7740d
Максим Блинов <126991331+maskon@users.noreply.github.com>
2023-07-20 10:42:54 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -34,14 +34,14 @@ try {
   // Настройки вашей почты
   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
   $mail->Username   = 'maksblinov80@gmail.com'; // Логин на почте
-  $mail->Password   = 'fwqkjgkgsdrthemb'; // Пароль на почте
+  $mail->Password   = 'fwqk jgkg sdrt hemb'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
   $mail->setFrom('maksblinov80@gmail.com', 'Заявка с вашего сайта'); // Адрес самой почты и имя отправителя
 
   // Получатель письма
-  $mail->addAddress('maksblinov80@gmail.com');
+  $mail->addAddress('maskon12@yandex.ru');
 
   // Прикрипление файлов к письму
   if (!empty($file['name'][0])) {
